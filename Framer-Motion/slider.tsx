735a74026d391c2d708c45d1816c052486dc9ff0
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-01-18 16:39:13 +0900

    Create slider.tsx

---
+++ Framer-Motion/slider.tsx
@@ -0,0 +1,149 @@
+import styled, { createGlobalStyle } from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+export const Global = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+  
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    line-height: 1.2;
+    font-weight:300;
+    color: black; 
+    
+  }
+  a {
+    text-decoration:none;
+    color:inherit;
+  }
+`;
+
+const Wrapper = styled(motion.div)`
+  width:100vw;
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position:absolute;
+  top:100px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const box = {
+  entry: (back:boolean) => {
+    return {
+      x: back?-500:500,
+      opacity: 0,
+      scale: 0,
+  }},
+  center: (back:boolean) => {
+    return {
+      x:0,
+      opacity:1,
+      scale:1,
+      transition: {
+      duration: 0.3,
+    }}
+  },
+  exit: (back:boolean) => {
+    return {
+      x: back?500:-500,
+      opacity:0,
+      scale:0,
+      transition:{
+      duration:0.3,
+    }}
+  }
+}
+
+function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev+1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev-1));
+  };
+  return (
+    //AnimatePresence는 항상 Visible 해야함
+    //key가 없다면 react.js 는 element가 사라졌다고 생각함
+    <Wrapper>
+      <AnimatePresence exitBeforeEnter custom={back}>
+       <Box
+          custom={back}
+          variants={box} 
+          key={visible}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          >{visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
+    </Wrapper>
+  );
+
+}
+export default App;
