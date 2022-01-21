afa5c1a99d498c378b90469f1f5cdaea3aa777e6
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-01-21 09:14:17 +0900

    Update Search.tsx

--- NomFlixClone/src/Routes/Search.tsx
+++ NomFlixClone/src/Routes/Search.tsx
@@ -1,6 +1,17 @@
-function Search() {
+import { useLocation } from "react-router-dom";
+import { searchMovies } from "../api";
 
-    return null;
+//URLSearchParams('params')
+function Search() {
+    const location = useLocation(); //location을 이용하면 지금 있는 곳의 정보를 얻을 수 있음
+    const keyword = location.search.slice(9);
+    const params = new URLSearchParams(location.search);
+    const movie = searchMovies(keyword);
+    console.log(movie);
+    return (
+    <>
+        <h1>{params.get('keyword')}</h1>
+    </>);
 }
 
 export default Search;
