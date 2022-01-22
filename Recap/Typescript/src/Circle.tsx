9a11604eb8728ab621301c0874e2b5d2623888b1
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-01-22 15:16:03 +0900

    Update Circle.tsx

--- Recap/Typescript/src/Circle.tsx
+++ Recap/Typescript/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface CircleProps {
@@ -16,7 +17,9 @@ const Container = styled.div<CircleProps>`
 `;
 
 // optional 일 때  props에 ??를 통해서 default 값을 넘겨줄 수 있다.
+// state가 number일수도 있고 string일 수 있다 <number | string>
 function Circle({bgColor, borderColor, text = "Default value (syntx ES6)"}:CircleProps) {
+    const [counter, setCounter] = useState<number|string>(0);
     return(
         <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
             {text}
