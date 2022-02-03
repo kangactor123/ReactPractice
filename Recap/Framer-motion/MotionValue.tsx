658a2b8545eec5487b980b2e7f3814387e4f41fa
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-02-03 14:07:00 +0900

    Create MotionValue.tsx

---
+++ Recap/Framer-motion/MotionValue.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+
+const Wrapper = styled(motion.div)`
+  width: 100vw;
+  height: 200vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
+};
+
+
+//scroll을 추적, x y 좌표를 추적
+function App() {
+  //motion value가 update 될 때 rendering이 일어나지 않는다.
+  //useViewportScroll scroll의 motion value를 넘겨준다.
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]); //scroll moving을 mapping 중
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  return (
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    </Wrapper>
+  );
+}
+
+export default App;
