d2ed21c41d83e9a50fa8818711a1f311449cfcbb
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-02-03 11:38:09 +0900

    Create Drag.tsx

---
+++ Recap/Framer-motion/Drag.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
+};
+
+//색상을 animation 하려면 정수값으로 줘야함
+//drag="x" x축으로만 가능하게 잠궈버림
+//ref를 만들어서 dragConstraints를 걸어버림
+function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  return (
+    <Wrapper>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
+    </Wrapper>
+  );
+}
+
+export default App;
