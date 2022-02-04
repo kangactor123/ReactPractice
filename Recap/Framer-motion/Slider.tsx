891f56c8c36124e9b352c56a471f52c210e6fbf9
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-02-04 10:01:44 +0900

    Create Slider.tsx

---
+++ Recap/Framer-motion/Slider.tsx
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const box = {
+  entry: (back: boolean) => {
+    return {
+      x: back ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+    };
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  exit: (back: boolean) => {
+    return {
+      x: back ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+    };
+  },
+};
+
+//AnimationPresence는 항상 visible, 내부에는 condition이 있어야한다
+//custom사용하려면 variants에서 object를 return하는 function으로 해줘야함
+//custom을 사용하려면 AnimatePresence에도 넣어줘야함
+function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  //key만 바꿔주는 것으로 react.js는 element가 사라졌다고 판단한다.
+  //key를 바꾸면 component를 re-rendering 해준다.
+  return (
+    <Wrapper>
+      <AnimatePresence exitBeforeEnter custom={back}>
+        <Box
+          custom={back}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
+    </Wrapper>
+  );
+}
+
+export default App;
