d432acc196ad1550d4c18b7387a45bdef9e8b4bb
THOMAR <82820237+kangactor123@users.noreply.github.com>
2022-02-22 10:13:14 +0900

    Update useConfirm.js

--- hooks/useConfirm.js
+++ hooks/useConfirm.js
@@ -1,14 +1,17 @@
 import { useEffect, useRef } from "react";
 
-const useConfirm = (message = "", callback, rejection) => {
-  if (typeof callback !== "function") {
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onCancel !== "function") {
     return;
   }
   const confirmAction = () => {
     if (confirm(message)) {
-      callback();
+      onConfirm();
     } else {
-      rejection();
+      onCancel();
     }
   };
   return confirmAction;
